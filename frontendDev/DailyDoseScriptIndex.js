70f13ccb0fc70910a19c613fb35f5833c7aa799c
LemonadeIcedTea <samueldyreyes@gmail.com>
2025-07-02 09:17:32 +0800

    added a html, css and js (gonna update them as we progress)

---
+++ frontendDev/DailyDoseScriptIndex.js
@@ -0,0 +1,11 @@
+const bookmark = document.getElementById("bookmark");
+const receivePositivity = document.getElementById("receive-positivity");
+const addAffirmation = document.getElementById("add-affirmation");
+
+const positiveMessages = [];
+
+function displayPositiveMessage() {
+    //something here
+}
+
+receivePositivity.addEventListener("click", displayPositiveMessage);
