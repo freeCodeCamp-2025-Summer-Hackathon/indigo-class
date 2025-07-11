caad63da32521b22d778e1136f06d8b3714da547
babanini95 <arobbanii5@gmail.com>
2025-07-11 09:28:19 +0800

    feat: add a route to create an affirmation

--- app/controllers/root.py
+++ app/controllers/root.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from typing import List
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 
+from app import db
 from app.models import User, Affirmation
 
 root_bp = Blueprint("root", __name__)
@@ -60,3 +61,27 @@ def affirmations():
     """
     all_affirmations: List[Affirmation] = Affirmation.query.all()
     return render_template("affirmations/index.html", all_affirmations=all_affirmations)
+
+
+@root_bp.route("/affirmations/add", methods=["GET", "POST"])
+@login_required
+def add_affirmation():
+    """
+    Add an affirmation
+    """
+    if request.method == "POST":
+        text: str | None = request.form.get("affirmation_text")
+
+        if not text:
+            flash("Please type your affirmation", "error")
+            return render_template("affirmations/add.html")
+
+        affirmation = Affirmation(affirmation_text=text, user_id=current_user.user_id)
+
+        db.session.add(affirmation)
+        db.session.commit()
+
+        flash("Your affirmation have been added", "success")
+        return redirect(url_for("root.affirmations"))
+
+    return render_template("affirmations/add.html")
