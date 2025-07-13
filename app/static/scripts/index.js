c0f90656bcf6e66bcf6a68cba8ffaec95f5636a5
Thy-xin <xinxinaug@gmail.com>
2025-07-14 02:56:20 +0800

    feat: new footer and header

--- app/static/scripts/index.js
+++ app/static/scripts/index.js
@@ -1,1 +1,14 @@
+// Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
+function navDropdown() {
+    const navDropdown = document.getElementById("nav__dropdown");
+    if (navDropdown.style.visibility === "visible") {
+        navDropdown.style.visibility = "hidden";
+    } else {
+        navDropdown.style.visibility = "visible";
+    }
+  } 
 
+// To top button in footer
+function toTop() {
+    window.scrollTo(0, 0);
+  } 
